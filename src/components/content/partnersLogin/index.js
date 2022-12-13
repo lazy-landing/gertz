1a72a92e881d377ae1d940630e0b422b6006b1e7
mtrunov <misha_trunov@hotmail.com>
2022-12-13 19:56:37 +0200

    added warning for partners

--- src/components/content/partnersLogin/index.js
+++ src/components/content/partnersLogin/index.js
@@ -1,21 +1,21 @@
 import styles from './styles.module.scss';
-import { useEffect, useRef, useState } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useRef, useState } from 'react';
 
 const PartnersLogin = () => {
 	const ref = useRef(null);
-	const { pathname } = useLocation();
 	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
-
-	useEffect(() => {
-		if (pathname === '/partners') {
-			ref?.current?.scrollIntoView();
-		}
-	}, [pathname]);
+	const [emailWarning, setEmailWarning] = useState(false);
+	const [passwordWarning, setPasswordWarning] = useState(false);
+	const [userWarning, setUserWarning] = useState(false);
 
 	const onChange = (e) => {
 		const { target: { type, value } } = e;
+
+		setUserWarning(false);
+		setEmailWarning(false);
+		setPasswordWarning(false);
+
 		if (type === 'email') {
 			setEmail(value);
 		}
@@ -24,13 +24,27 @@ const PartnersLogin = () => {
 		}
 	};
 
-	const isDisabled = () => {
-		return !email || !password;
+	const onSubmit = (e) => {
+		e.preventDefault();
+		setEmailWarning(Boolean(!email));
+		setPasswordWarning(Boolean(!password));
+
+		if (email && password) {
+			setUserWarning(true);
+		}
 	};
 
 	return (
 		<div ref={ref} className={styles.contact}>
+			<div className={userWarning ? styles['error-container'] : styles.hidden}>
+				<span className={styles['user-warning']}>
+					Looks like you are not our partner yet
+				</span>
+			</div>
 			<form className={styles.form}>
+				<span className={emailWarning ? styles.warning : styles.hidden}>
+						* Email is a required field
+				</span>
 				<input
 					type='email'
 					className={`${styles.input} ${styles['input-margin']}`}
@@ -38,6 +52,9 @@ const PartnersLogin = () => {
 					value={email}
 					onChange={onChange}
 				/>
+				<span className={passwordWarning ? styles.warning : styles.hidden}>
+						* Password is a required field
+				</span>
 				<input
 					type='password'
 					className={`${styles.input} ${styles['input-margin']}`}
@@ -46,8 +63,8 @@ const PartnersLogin = () => {
 					onChange={onChange}
 				/>
 				<button
-					disabled={isDisabled()}
 					className={`${styles.button} ${styles['input-margin']}`}
+					onClick={onSubmit}
 				>
 					Login
 				</button>
--- src/index.js
+++ src/index.js
@@ -1,18 +1,38 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
+import styles from './styles.module.scss';
+import Header from './components/header';
+import Home from './components/content/home';
+import About from './components/content/about';
+import Contact from './components/content/contact';
+import PartnersLogin from './components/content/partnersLogin';
+import Footer from './components/footer';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
+const getMainContent = () => (
+	<>
+		<Home />
+		<About />
+		<Contact />
+	</>
+);
+
 root.render(
-	<StrictMode>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
-	</StrictMode>
+	<BrowserRouter>
+		<div className={styles.App}>
+			<Header />
+			<div className={styles.content}>
+				<Routes>
+					<Route path="*" element={getMainContent()} />
+					<Route path="/partners" element={<PartnersLogin />} />
+				</Routes>
+			</div>
+			<Footer />
+		</div>
+	</BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
